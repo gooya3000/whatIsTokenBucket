a5f2000c9993eda06df02958d1e028edcd1e71a9
gooya3000 <withthdud0923@gmail.com>
2025-10-30 21:20:06 +0900

    initial commit

---
+++ src/main/java/com/example/whatistokenbucket/WhatIsTokenBucketApplication.java
@@ -0,0 +1,13 @@
+package com.example.whatistokenbucket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatIsTokenBucketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhatIsTokenBucketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/whatistokenbucket/WhatIsTokenBucketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.whatistokenbucket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatIsTokenBucketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
